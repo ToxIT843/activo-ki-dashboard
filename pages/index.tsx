cdc2429944041de39d336c372c0ce875853a9a82
ToxIT843 <95283538+ToxIT843@users.noreply.github.com>
2025-08-07 22:45:26 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,9 @@
 export default function Home() {
-  return <div>Welcome to Activo-KI Dashboard</div>;
+  return (
+    <div>
+      <h1>Willkommen im Activo-KI Dashboard</h1>
+    </div>
+  );
 }
+
 
